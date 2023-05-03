c5ddf4976333eab62580e6b89e01933afb6760fd
ynan-dan <131932726+ynan-dan@users.noreply.github.com>
2023-05-03 22:42:37 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -12,7 +12,7 @@ except ImportError as e:
 def ridge(data):
     x, y = read_data()
     i = np.eye(6)
-    alpha = 0.0015
+    alpha = 1e-18
     weight = np.dot(np.linalg.inv(np.dot(x.T, x) + np.dot(alpha, i)), np.dot(x.T, y))
     print(np.dot(alpha, i))
     return data @ weight
